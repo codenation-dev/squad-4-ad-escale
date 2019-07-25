ff06c860e2f1b07af24a159e489d8b7631b31c3a
Araujo <stefanielima_araujo@hotmail.com>
2019-07-25 14:56:23 -0300

    react initial commit

---
+++ pets-front/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './home.css'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+}
+
+export default Home
