6d826454a85e47eada0019a7cd43f0a21334212a
Araujo <stefanielima_araujo@hotmail.com>
2019-08-02 00:56:03 -0300

    Add menu responsivo

--- pets-front/src/components/Header/index.js
+++ pets-front/src/components/Header/index.js
@@ -5,16 +5,37 @@ import logopetcodes from '../../_assets/img/logo-petcodes.png'
 
 class Header extends Component {
     render() {
+
+        this.openMenu = () => {
+            document.documentElement.classList.add('menu-ativo');
+        }
+
+        this.closeMenu = () => {
+            document.documentElement.classList.remove('menu-ativo');
+        }
+
         return (
-            <nav className="navbar navbar-fixed-top navbar-inverse ">
-                <div className="container">
-                    <div className="navbar-header header">
-                        <div href="#" className="navbar-brand">
-                            <img src={logopetcodes} alt="Logo" className="navbar-logo" />
-                        </div>
-                    </div>
+            <div className="nav-bar">
+                <button type="button"
+                    className="btn btn-default btn-lg menu-abrir"
+                    onClick={this.openMenu}>
+                    Menu
+                </button>
+                <div href="#" className="navbar-brand">
+                    <img src={logopetcodes} alt="Logo" className="navbar-logo" />
                 </div>
-            </nav>
+                <nav className="barra-nav">
+                    <button type="button"
+                        className="btn btn-default btn-lg menu-fechar"
+                        onClick={this.closeMenu}>
+                        X
+                    </button>
+                    <ul className="menu-principal">
+                        <li><a href="#">Entrar</a></li>
+                        <li><a href="#">Cadastrar</a></li>
+                    </ul>
+                </nav>
+		</div>
         )
     }
 }
