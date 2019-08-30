9727fec707b31f6d9b8354d595903e242645ff71
Araujo <stefanielima_araujo@hotmail.com>
2019-08-30 18:35:13 -0300

    Add buttons

--- pets-front/src/pages/MyPets/index.js
+++ pets-front/src/pages/MyPets/index.js
@@ -2,19 +2,25 @@ import React, { Component } from "react";
 import PetCard from "../../components/PetCard";
 import { connect } from "react-redux";
 import { getMyPets as actionGetMyPets, selectCard } from "./actions";
+import "./my-pets.css";
 
 const MyPets = ({ myPets, handleSelectedCard }) => {
   const onSelectedCard = myPets => {
     handleSelectedCard(myPets);
   };
 
-  console.log(myPets);
   return (
     <div className="div-center">
-      <h2>Meus Pets Cadastrados</h2>
+      <h2 className="page-title">Meus Pets Cadastrados</h2>
       <div className="all-cards">
         {myPets.map(pet => (
-          <PetCard key={pet.id} pet={pet} />
+          <div className="my-pet-card">
+            <PetCard key={pet.id} pet={pet} />
+            <div className="buttons">
+              <button className="delete">X</button>
+              <button className="edite">Editar</button>
+            </div>
+          </div>
         ))}
       </div>
     </div>
